33c353a0f76535e12b52021051939d4b016f150a
ObiDuck <obiduckmail@gmail.com>
2023-02-12 20:04:46 +0100

    Check Credentials

--- lib/character_page.dart
+++ lib/character_page.dart
@@ -2,6 +2,7 @@ import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CharacterPage extends StatelessWidget {
   const CharacterPage({super.key});
@@ -9,7 +10,11 @@ class CharacterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[100]!,
       body: Column(children: [
+        Text('Character Name',
+            style: GoogleFonts.poiretOne(
+                fontSize: 26, color: const Color.fromARGB(255, 97, 18, 7))),
         ElevatedButton(
           onPressed: () {
             // Navigate back to first route when tapped.
